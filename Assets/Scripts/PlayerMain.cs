b24b507de426c0f1df549e3fb6f359ed2451e81d
ctoloza499 <50080534+ctoloza499@users.noreply.github.com>
2019-11-02 11:39:58 -0500

    movimiento corregido

---
+++ Assets/Scripts/PlayerMain.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine ;
+using UnityEngine.InputSystem;
+
+public class PlayerMain : MonoBehaviour
+{
+    private CharacterController2D _controller;
+    private float _horizontal;
+    private bool _crouch = false;
+    private bool _jump = false;
+    [SerializeField] private float _walkSpeed = 40f;
+
+
+    void OnHorizontal(InputValue value)
+    {
+        _horizontal = value.Get<float>();
+        Debug.Log("hor" + _horizontal);
+    }
+
+    void OnJump()
+    {
+        _jump = true;
+    }
+
+    void OnCrouch(InputValue value)
+    {
+        //_crouch = true;
+        Debug.Log("crouch" );
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        _controller.Move(_horizontal * Time.fixedDeltaTime * _walkSpeed, _crouch, _jump);
+        _jump = false;
+    }
+}
