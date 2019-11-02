73d6899cee058389cd35cfcc8f1794795d1db417
MrDooF07 <yaramirez68@gmail.com>
2019-11-02 15:14:30 -0500

    Mechanics paint

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -7,58 +7,72 @@ public class Platform : MonoBehaviour
     [SerializeField]
     private GameObject objectAttached;
 
-    private SpriteRenderer renderObjectAttached;
-    private BoxCollider boxColliderObject;
-    private Color silhouetteObjectAttached;
-    private Color fullColor;
-    private bool hasCollider = false;
+
+    public SpriteRenderer renderObjectAttached;
+    public BoxCollider2D boxColliderObject;
+    public Color unAlphaObject;
+    public Color silhouetteObjectAttached;
+    public Color fullColorObject;
+    public bool objectHasCollider = false;
+    public bool objectIsPainted = false;
+    public string nameObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        silhouetteObjectAttached = Color.black;
-        fullColor = new Color(255, 255, 255);
+        renderObjectAttached = objectAttached.GetComponent<SpriteRenderer>();
+        silhouetteObjectAttached = new Color(0,0,0,255);
+        fullColorObject = new Color(255, 255, 255);
+        unAlphaObject = new Color(255, 255, 255, 0);
+        renderObjectAttached.color = unAlphaObject;
+        nameObject = objectAttached.name;
     }
 
     // Update is called once per frame
     void Update()
     {
-        try
-        {
-            renderObjectAttached = objectAttached.GetComponent<SpriteRenderer>();
-        }
-        catch (System.Exception)
-        {
-            throw;
-        }
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (objectIsPainted == false && collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Aca entra");
+                renderObjectAttached.color = silhouetteObjectAttached;
+            
+        }
+            
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
         if (Input.GetKeyDown(KeyCode.Z))
         {
             Paint();
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        if (!hasCollider)
+        if (!objectIsPainted)
         {
-            boxColliderObject = objectAttached.AddComponent(typeof(BoxCollider)) as BoxCollider;
-            hasCollider = true;
+            renderObjectAttached.color = unAlphaObject;
         }
-        foreach (ContactPoint contact in collision.contacts)
-        {
-            objectAttached.SetActive(true);
-            renderObjectAttached.color = silhouetteObjectAttached;
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        objectAttached.SetActive(false);
     }
 
     public void Paint()
     {
-        renderObjectAttached.color = fullColor;
+        if (objectAttached.name == nameObject)
+        {
+            if (!objectHasCollider)
+            {
+                boxColliderObject = objectAttached.AddComponent(typeof(BoxCollider2D)) as BoxCollider2D;
+                objectHasCollider = true;
+            }
+            renderObjectAttached.color = fullColorObject;
+            objectIsPainted = true;
+        }
     }
+
 }
