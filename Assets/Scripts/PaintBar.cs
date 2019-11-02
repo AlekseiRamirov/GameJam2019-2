00fc60d466392ebc786a3d47d32931abe221b042
MrDooF07 <yaramirez68@gmail.com>
2019-11-02 16:11:27 -0500

    Mechanic Reload Scene

--- Assets/Scripts/PaintBar.cs
+++ Assets/Scripts/PaintBar.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PaintBar : MonoBehaviour
 {
     [SerializeField] private Scrollbar _paintBar;
     [SerializeField] private float _paint = 100;
     private float _paintBody;
+    public Scene currentScene;
+    public string nameScene;
 
     void Start()
     {
+        currentScene = SceneManager.GetActiveScene();
+        nameScene = currentScene.name;
         _paintBody = _paint;
     }
 
@@ -18,6 +23,19 @@ public class PaintBar : MonoBehaviour
     {
         _paintBody -= lose;
         _paintBar.size = _paintBody / _paint;
+    }
+
+    public float GetQuantityPaint()
+    {
+        return _paintBody;
+    }
+
+    public void GameOver()
+    {
+        if (GetQuantityPaint()<1)
+        {
+            SceneManager.LoadScene(nameScene);
+        }
     }
 
 
