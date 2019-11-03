dc0e2d350f3f2d4b795c3126c18300f1c7371ae3
ctoloza499 <50080534+ctoloza499@users.noreply.github.com>
2019-11-02 21:26:29 -0500

    plataformas moviles

--- Assets/Scripts/PlayerMain.cs
+++ Assets/Scripts/PlayerMain.cs
@@ -9,6 +9,7 @@ public class PlayerMain : MonoBehaviour
     private float _horizontal;
     private bool _crouch = false;
     private bool _jump = false;
+    private Transform player;
     [SerializeField] private bool active = true;
     [SerializeField] private float _walkSpeed = 40f;
 
@@ -25,12 +26,6 @@ public class PlayerMain : MonoBehaviour
             _jump = true;
         }   
     }
-
-    /*void OnCrouch(InputValue value)
-    {
-        _crouch = (int)value.Get<float>() != 0;
-
-    }*/
     void OnChange()
     {
         active = !active;
@@ -39,6 +34,7 @@ public class PlayerMain : MonoBehaviour
     void Start()
     {
         _controller = GetComponent<CharacterController2D>();
+        player = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -54,6 +50,22 @@ public class PlayerMain : MonoBehaviour
             // Move our character
             _controller.Move(_horizontal * Time.fixedDeltaTime * _walkSpeed, _crouch, _jump);
             _jump = false;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            player.transform.parent = collision.transform;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            player.transform.parent = null;
         }
     }
 }
